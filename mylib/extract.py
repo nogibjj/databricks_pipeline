2f928995cdb19338e86cd88ca99a34efdd6002e6
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-10-08 15:28:48 -0700

    Update extract.py

--- mylib/extract.py
+++ mylib/extract.py
@@ -1,11 +1,27 @@
+import os
 import requests
+import pandas as pd
+
 
 def extract(
-    url="https://raw.githubusercontent.com/plotly/datasets/master/diabetes.csv",
-    file_path="Diabetes.csv",
+    url="""
+    https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2021-financial-year-provisional/Download-data/annual-enterprise-survey-2021-financial-year-provisional-csv.csv
+    """,
+    url2="""
+    https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2021-financial-year-provisional/Download-data/annual-enterprise-survey-2021-financial-year-provisional-size-bands-csv.csv
+    """,
+    file_path="data/table1.csv",
+    file_path2="data/table2.csv",
+    directory="data",
 ):
     """Extract a url to a file path"""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     with requests.get(url) as r:
         with open(file_path, "wb") as f:
             f.write(r.content)
-    return file_path
+    with requests.get(url2) as r:
+        with open(file_path2, "wb") as f:
+            f.write(r.content)
+    
+    return file_path, file_path2
